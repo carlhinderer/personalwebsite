7e5531deb9227284a832311ab4b974997c44c39c
Carl Hinderer <carl.hinderer4@gmail.com>
2014-06-18 12:48:40 -0400

    Changed database.yml and secret_token.rb to grab values from environment variables.  Fixed logout bug to use delete method instead of get.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Personalwebsite::Application.config.secret_key_base = 'b1059f014cdf81c57d14423a6a817f86fd763370b43a74d13bb7a1434639c95ee5c73afbfb26f51fd7ade3ba56951616f6299d344b3c84648a8e72730fe29016'
+Personalwebsite::Application.config.secret_key_base = ENV["SECRET_KEY"]
