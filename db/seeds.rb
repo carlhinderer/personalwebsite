9ea7d231459952f388c512b516e8b6a7db1a7a65
Carl Hinderer <carl.hinderer4@gmail.com>
2014-06-20 15:26:40 -0400

    Added blog post categories to db seeds file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,7 @@ admin = Admin.find_or_create_by!(:email => ENV["ADMIN_EMAIL"]) do |admin|
   admin.password = ENV["ADMIN_PASSWORD"]
   admin.password_confirmation = ENV["ADMIN_PASSWORD"]
 end
+
+# Add blog post categories
+Category.find_or_create_by!(:name => 'Programming')
+Category.find_or_create_by!(:name => 'Personal')
